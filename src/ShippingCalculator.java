ff0be6b2122e46527afe65b5988eff4a86cdeea9
Valdemaras <valdemarasged@gmail.com>
2023-08-02 22:17:18 +0300

    nested calculateShipping method with a coupon discount

--- src/ShippingCalculator.java
+++ src/ShippingCalculator.java
@@ -5,14 +5,16 @@ public class ShippingCalculator {
     boolean isFilled;
     double billAmount;
     String shipping;
+    String couponCode;
 
-    public ShippingCalculator(boolean filled, double cost, String shippingMethod) {
+    public ShippingCalculator(boolean filled, double cost, String shippingMethod, String coupon) {
         if (cost > 24.00) {
             System.out.println("High value item!");
         }
         isFilled = filled;
         billAmount = cost;
         shipping = shippingMethod;
+        couponCode = coupon;
     }
 
     public void ship() {
@@ -30,7 +32,12 @@ public class ShippingCalculator {
             return 0;
         }
         else if(shipping.equals("Express")){
-            return 1.75;
+            if (couponCode.equals("ship50")){
+                return 0.85;
+            }
+            else {
+                return 1.75;
+            }
         }
         else {
             return 0.50;
@@ -38,9 +45,8 @@ public class ShippingCalculator {
     }
 
     public static void main(String[] args) {
-        // do not alter the main method!
-        ShippingCalculator book = new ShippingCalculator(true, 9.99, "Express");
-        ShippingCalculator chemistrySet = new ShippingCalculator(false, 72.50, "Regular");
+        ShippingCalculator book = new ShippingCalculator(true, 9.99, "Express", "ship50");
+        ShippingCalculator chemistrySet = new ShippingCalculator(false, 72.50, "Regular", "Regular");
 
         book.ship();
         chemistrySet.ship();
