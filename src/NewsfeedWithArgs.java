e9934c2085bf1b74791c2e9e5bc50d3c3f015ea9
Valdemaras <valdemarasged@gmail.com>
2023-08-05 00:02:12 +0300

    arrays. Accessing to main args array, from terminal

---
+++ src/NewsfeedWithArgs.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+/*
+Accessing to args array, this function are called from terminal!
+Two values "Robot" and "Human"
+ */
+public class NewsfeedWithArgs {
+
+    String[] topics;
+
+    public NewsfeedWithArgs(String[] initialTopics) {
+        topics = initialTopics;
+    }
+
+    public static void main(String[] args) {
+        NewsfeedWithArgs feed;
+        if (args[0].equals("Human")) {
+
+            //topics for a Human feed:
+            String[] humanTopics = {"Politics", "Science", "Sports", "Love"};
+            feed = new NewsfeedWithArgs(humanTopics);
+
+        } else if(args[0].equals("Robot")) {
+
+            //topics for a Robot feed:
+            String[] robotTopics = {"Oil", "Parts", "Algorithms", "Love"};
+            feed = new NewsfeedWithArgs(robotTopics);
+
+        } else {
+            String[] genericTopics = {"Opinion", "Tech", "Science", "Health"};
+            feed = new NewsfeedWithArgs(genericTopics);
+        }
+
+        System.out.println("The topics in this feed are:");
+        System.out.println(Arrays.toString(feed.topics));
+    }
+}
+
