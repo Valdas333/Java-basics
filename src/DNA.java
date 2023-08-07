343cb6b020ee945f7989c980be8b3023d085d067
Valdemaras <valdemarasged@gmail.com>
2023-08-07 22:26:35 +0300

    revised DNA function, one more strand to test, and a more accurate search algortim

--- src/DNA.java
+++ src/DNA.java
@@ -1,14 +1,19 @@
 public class DNA {
     /*
-    Function that determines is strand of DNA protein or not.
+    A function that determines whether a strand of DNA is a protein or not.
+    A protein is defined between the "ATG" and "TGA" char's and is not longer than 3 char.
+    Function also checks if "ATG" is not before "TGA".
      */
     public boolean isProtein(String strand){
-        if(strand.length()% 3 != 0){
+        int proteinStartATG = strand.indexOf("ATG");
+        int proteinEndATG = proteinStartATG + 3;
+        int proteinStartTGA = strand.indexOf("TGA");
+
+        if(strand.length() % 3 != 0 || proteinStartATG < 0 || proteinStartTGA < 0 || proteinStartATG + 3 > proteinStartTGA) {
             return false;
-        } else if ((strand.substring(0, 3).equals("ATG")) && (strand.substring(strand.length()-3, strand.length()).equals("TGA"))) {
-            return true;
         }
-        return false;
+        System.out.println(strand.substring(proteinEndATG,proteinStartTGA));
+            return true;
     }
 
 
@@ -17,9 +22,11 @@ public class DNA {
     String dna1 = "ATGCGATACGCTTGA";
     String dna2 = "ATGCGATACGTGA";
     String dna3 = "ATTAATATGTACTGA";
+    String dna4 = "TGAYURDGGHFDATG";
     System.out.println(strand1.isProtein(dna1));
     System.out.println(strand1.isProtein(dna2));
     System.out.println(strand1.isProtein(dna3));
+    System.out.println(strand1.isProtein(dna4));
     }
 }
 
